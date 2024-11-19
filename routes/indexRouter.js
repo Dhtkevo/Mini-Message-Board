be1bea5c366e23a3043e1ce0649c137995a8cb27
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2024-11-18 23:23:53 -0500

    Implement message detail functionality

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -16,7 +18,29 @@ const messages = [
 const indexRouter = Router();
 
 indexRouter.get("/new", (req, res) => {
-  console.log("Fill in later");
+  res.render("form");
+});
+
+indexRouter.get("/:id", (req, res) => {
+  const id = Number(req.params["id"]);
+  let message = null;
+  for (let msg of messages) {
+    if (msg.id === id) {
+      message = msg;
+      break;
+    }
+  }
+
+  res.render("details", { message: message });
+});
+
+indexRouter.post("/new", (req, res) => {
+  const { message } = req.body;
+  const { userName } = req.body;
+  const date = new Date();
+
+  messages.push({ user: userName, text: message, added: date });
+  res.redirect("/");
 });
 
 indexRouter.get("/", (req, res) => {
