af4df0aefe1de7c180b0ae28795a10b59ab52cf5
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2024-11-19 18:30:18 -0500

    Modify package.json for deployment

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express();
 const indexRouter = require("./routes/indexRouter");
 const path = require("node:path");
 
+const PORT = process.env.PORT || 3000;
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
